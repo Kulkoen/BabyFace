b6162f1eb3781a4a8b8a380816f49e9bc2e0e6da
Phuc Tran <phuc.d.tran.24@dartmouth.edu>
2023-11-18 18:38:55 -0500

    Baisc Subscriber working

--- dev/conversationer.py
+++ dev/conversationer.py
@@ -14,7 +14,7 @@ INPUT_FILE = "input.txt"
 
 # Defaults
 context_str = ""
-input_str = ""
+input_str = {}
 
 def init_sample(
     project: None,
@@ -39,8 +39,11 @@ def init_sample(
     )
     
 def main():
+    global input_str
+
     # Initialize Redis database 
     r = create_redis_instance()
+    user_sub = r.pubsub()
     
     # Initialize Model
     vertexai.init(project="ai-atl-demo", location="us-central1")
@@ -56,18 +59,38 @@ def main():
     # Initialize Inputs
     with open(CONTEXT_FILE) as f:
         context_str = f.read()
-    with open(CONTEXT_FILE) as f:
-        input_str = f.read()
+    # with open(CONTEXT_FILE) as f:
+    #     input_str = f.read()
     
     # Conversation
     chat = chat_model.start_chat(
         context=context_str
     )
-    response = chat.send_message(input_str, **parameters)
-    print(f"Response from Model: {response.text}")
 
-    while True:
-        r.publish('chat-response', response.text)
+    try:
+        # Try to ping the Redis server to check the connection
+        response = r.ping()
+        print("Connection successful. Redis server responded:", response)
+        user_sub.subscribe("user-query")
+
+        # Publish Message
+        while True:
+            # Listens to the message
+            for message in user_sub.listen():
+                if message['type'] == 'message':
+                    input_str = message
+                    break
+            
+            # Publish and print response
+            if input_str:
+                print(input_str['data'])
+                response = chat.send_message(str(input_str['data']), **parameters)
+                print(f"Response from Model: {response.text}")
+                # r.publish('chat-response', response.text)
+                break
+
+    except redis.exceptions.ConnectionError as e:
+        print("Error connecting to Redis:", e)
     
 if __name__ == "__main__":
     main()
--- dev/redis_database.py
+++ dev/redis_database.py
@@ -2,8 +2,8 @@ import redis
 
 def create_redis_instance():
     r = redis.Redis(
-    host='',
+    host='redis-17203.c259.us-central1-2.gce.cloud.redislabs.com',
     port=17203,
-    password='')
+    password='kWFTLkM3O10DSUeCC1sjhKxtrGdJpvyf')
 
     return r 
