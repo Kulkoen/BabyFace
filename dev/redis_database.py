185c9af5e656b54aeb16132614afb2274671a94b
Phuc Tran <phuc.d.tran.24@dartmouth.edu>
2023-11-21 14:09:50 -0500

    deleted repeated files

--- dev/redis_database.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-def create_redis_instance():
-    r = redis.Redis(
-    host='redis-17203.c259.us-central1-2.gce.cloud.redislabs.com',
-    port=17203,
-    password='kWFTLkM3O10DSUeCC1sjhKxtrGdJpvyf')
-
-    return r 
